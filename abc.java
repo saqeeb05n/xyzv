94c30968df3cf926c483dd4a4072baaffadbe413
Saqeeb Noorashanavar <saqeeb05n@gmail.com>
2022-11-29 10:35:24 +0530

    First commit

---
+++ abc.java
@@ -0,0 +1,13 @@
+class abc
+{
+    public static void main (String arg[])
+    {
+        int a,b,c;
+        a=10;
+        b=4;
+        c=a+b;
+        {
+            System.out.println(c);
+        }
+    }
+}
